d4d1b870ec27e62d4e8896fa1b6a23c6e50e0447
Julian Baldwin <julianbaldwin97@gmail.com>
2023-10-04 00:06:45 -0500

    improvement/cleanup of day24

--- 2022/day24.py
+++ 2022/day24.py
@@ -7,53 +7,53 @@ input_file = "input24.txt"
 with open(input_file, "r") as file:
 	data = file.read().split("\n")
 
+dim = len(data), len(data[0])
+
+# parsing grid size and blizzards
 grid = {}
-bliz = []
+bliz = [] # tuples of (pos, direction)
+
+bliz_directions = {
+	">": (0,1),
+	"<": (0,-1),
+	"v": (1,0),
+	"^": (-1,0),
+}
 
 for i, line in enumerate(data):
 	for j, char in enumerate(line):
-		if char in [">", "<", "v", "^"]:
-			match char:
-				case ">":
-					d = (0,1)
-				case "<":
-					d = (0,-1)
-				case "v":
-					d = (1,0)
-				case "^":
-					d = (-1,0)
-			
-			bliz.append(((i,j), d))
-			char = "."
-		grid[(i,j)] = char
+		if char in bliz_directions:
+			bliz.append(((i,j), bliz_directions[char]))
+			grid[(i,j)] = "."
+		else:
+			grid[(i,j)] = char
 
-print_grid(grid)
-
+# all blizzards move one step (and wrap around if necessary)
 def sim_bliz(bliz):
 	result = []
 	for b in bliz:
 		pos, d = b
-
 		test = (pos[0] + d[0], pos[1] + d[1])
 		assert test in grid
+
+		# if we hit a wall, wrap around
 		if grid[test] != ".":
 			if d == (1, 0):
-				test = (min([x[0] for x in grid.keys() if x[1] == test[1] and grid[x] == "."]), test[1])
+				test = (1, test[1])
 			elif d == (-1, 0):
-				test = (max([x[0] for x in grid.keys() if x[1] == test[1] and grid[x] == "."]), test[1])
+				test = (dim[0] - 2, test[1])
 			elif d == (0, 1):
-				test = (test[0], min([x[1] for x in grid.keys() if x[0] == test[0] and grid[x] == "."]))
+				test = (test[0], 1)
 			elif d == (0, -1):
-				test = (test[0], max([x[1] for x in grid.keys() if x[0] == test[0] and grid[x] == "."]))
+				test = (test[0], dim[1] - 2)
 
 		assert grid[test] == "."
-
 		result.append((test, d))
 
 	return result
-	
+
+# computes the next step blizzard series, updates list of states and set of blizzard positions
 def compute_next_bliz(bliz_states, bliz_sets):
-	# print("precomputing blizzard", len(bliz_states))
 	b = sim_bliz(bliz_states[-1])
 	bliz_states.append(b)
 	bliz_sets.append(set([x[0] for x in b]))
@@ -61,6 +61,7 @@ def compute_next_bliz(bliz_states, bliz_sets):
 bliz_states = [bliz]
 bliz_sets = [set([x[0] for x in bliz])]	
 	
+# heuristic for A* search (manhattan distance)
 def h(point, goal):
 	return abs(point[0] - goal[0]) + abs(point[1] - goal[1])
 
@@ -75,14 +76,14 @@ def search(start, end, bliz_i=0):
 
 	seen = {(0, start)}
 
+	# exit search if we search >500k states
 	searched = 0
-
-	while not q.empty() and searched <= 100000:
-		if searched % 10000 == 0:
-			print(searched)
+	while not q.empty() and searched <= 500000:
 		searched += 1
 
 		_, steps, cur = q.get()
+		# only compute next blizzard state when necessary
+		# might search states out of order because we are using heuristic
 		while steps + 1 + bliz_i >= len(bliz_sets):
 			compute_next_bliz(bliz_states, bliz_sets)
 		cur_bliz = bliz_sets[steps + 1 + bliz_i]
@@ -90,30 +91,30 @@ def search(start, end, bliz_i=0):
 		if cur == end:
 			return steps
 
-		# print("current:", steps, cur)
-
 		for d in [(1,0), (0,1), (-1,0), (0,-1), (0,0)]:
 			test = (cur[0] + d[0], cur[1] + d[1])
-			# print("test:", test)
+
 			if test not in grid or grid[test] != ".":
-				# print(test, "not valid square")
 				continue
 			if test in cur_bliz:
-				# print(test, "in blizzard")
 				continue
 			if (steps + 1, test) in seen:
 				continue
 
 			guess = steps + 1 + h(test, end)
 
-			# print("guess:", test, guess, steps + 1)
-
 			q.put((guess, steps + 1, test))
 			seen.add((steps + 1, test))
 
-there = search(start, end)
-back = search(end, start, bliz_i=there)
-there2 = search(start, end, bliz_i=(there + back))
+	print("no path found")
+	return -1
 
-print(there, back, there2)
-print(there + back + there2)
+
+there = search(start, end)
+
+print("Part 1:", there)
+
+back = search(end, start, bliz_i=there)
+there_again = search(start, end, bliz_i=(there + back))
+
+print("Part 2:", there + back + there_again)
