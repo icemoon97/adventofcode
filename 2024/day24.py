06ac0d93b7695e24d5e931558f367691d0f5f471
Julian Baldwin <julianbaldwin97@gmail.com>
2024-12-24 20:48:51 -0800

    day24 part 2
    solved manually looking at individual output bits using print helper

--- 2024/day24.py
+++ 2024/day24.py
@@ -11,6 +11,42 @@ def compute(wires, key):
             return compute(wires, in1) ^ compute(wires, in2)
         case val:
             return val
+        
+def print_tree(wires, key, depth, max_width):
+    if not depth:
+        return
+    match wires[key]:
+        case (op, in1, in2):
+            print(f"{key:|>{max_width - depth}} - {in1} {op} {in2}")
+            print_tree(wires, in1, depth - 1, max_width)
+            print_tree(wires, in2, depth - 1, max_width)
+        case val:
+            print(f"{key:|>{max_width - depth}} - {val}")
+        
+def add(wires_original, x, y):
+    wires = wires_original.copy()
+
+    for i in itertools.count():
+        key = f"x{i:02d}"
+        if key not in wires:
+            break
+        wires[key] = x % 2
+        x //= 2
+    for i in itertools.count():
+        key = f"y{i:02d}"
+        if key not in wires:
+            break
+        wires[key] = y % 2
+        y //= 2
+
+    output = []
+    for i in itertools.count():
+        key = f"z{i:02d}"
+        if key not in wires:
+            break
+        output.append(compute(wires, key))
+
+    return int("".join(map(str, output[::-1])), 2)
 
 def main(input_path: Path):
     with open(input_path, "r") as file:
@@ -26,22 +62,56 @@ def main(input_path: Path):
 
         wires[out] = (op_name, in1, in2)
 
-    output = []
-    for i in itertools.count():
-        key = f"z{i:02d}"
-        if key not in wires:
-            break
-        output.append(compute(wires, key))
+    swaps = [("z05", "bpf"), ("z11", "hcc"), ("hqc", "qcw"), ("z35", "fdw")]
+    for s1, s2 in swaps:
+        temp = wires[s1]
+        wires[s1] = wires[s2]
+        wires[s2] = temp
 
-    print(output)
+    for shift in range(44):
+        test_x = 1 << shift
+        test_y = 1 << shift
+        expected = test_x + test_y
+        z = add(wires, test_x, test_y)
+        print("shift", shift, expected, z)
 
-    output_val = int("".join(map(str, output[::-1])), 2)
+        for i in itertools.count():
+            b1 = expected % 2
+            b2 = z % 2
+            # print(i, b1, b2)
+            if b1 != b2:
+                print(i, b1, b2)
+            expected //= 2
+            z //= 2
+            if expected <= 0:
+                break
 
-    print(output_val)
+
+    flat_swaps = []
+    for s in swaps:
+        flat_swaps.extend(s)
+    print(",".join(sorted(flat_swaps)))
+
+    # sus bits: 5, 11, 24, 35
+
+    # print_tree(wires, "z36", 5, 8)
+
+    # zo5
+    # bpf
+
+    # z11
+    # hcc
+
+    # hqc
+    # qcw
+
+    # z35
+    # fdw
+
 
 if __name__ == "__main__":
     DAY = 24
-    print("===== Tests =====")
-    main(f"tests/day{DAY:02}.txt")
+    # print("===== Tests =====")
+    # main(f"tests/day{DAY:02}.txt")
     print("===== Input =====")
     main(f"inputs/day{DAY:02}.txt")
